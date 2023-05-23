e2f1e57ad19f5ca45b7eaf8d1a01d57aa7cea485
Dominique Cavailhez <github@c92.fr>
2023-05-23 16:41:17 +0200

    On repart sur twentytwentythree pour 2023

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Chavil.gym.15
-Theme WordPress sur base twentyfifteen pour afficher les cours de la Gym Volontaire de Chaville
+# Chavil.gym.WP
+Theme WordPress sur base twentytwentythree pour afficher les cours de la Gym Volontaire de Chaville
+
+STATUT
+======
 EN COURS DE DEVELOPPEMENT
+
+INSTALL
+=======
+Sous domaine - SSL Let's Encrypt
+WP : install
+Copier ces fichiers dans /wp-content/themes/gym
+Supprimer toutes extension et thème autre que 23
+Installer "Leaflet Map", activer, mises à jour
+Installer "Block Editor Colors", ajouter le jaune #fff00
+Installer "Contact Form 7"
+Extensions -> Contact Form 7 -> Réglages
+Installer "Favicon by RealFaviconGenerator"
+Apparence -> Favicon (images/favicon.jpg)
+
+Pour chaque compte éditeur
+Options -> Préférences
+-> Mise en lumière
+-> Afficher les libellés texte des boutons
+
+TODO
+====
+Editeur : Style blocs liste en mode tablette ou mobile
+
+TAGS DANS LES PAGES
+===================
+<meta http-equiv="refresh" content="0;https">
+[leaflet-map lng="2.19712" lat="48.81788"][leaflet-marker]
+
+MISE EN SERVICE
+===============
+Changer url
+Changer préfixe base wpgym_
+
--- functions.php
+++ functions.php
@@ -1,157 +1,245 @@
 <?php
+$annee = 2023;
+$nom_jour = explode(
+    " ",
+    " lundi mardi mercredi jeudi vendredi samedi dimanche"
+);
+$nom_mois = explode(
+    " ",
+    " janvier fevrier mars avril mai juin" .
+        " juillet aout septembre octobre novembre décembre" .
+        " janvier fevrier mars avril mai juin"
+);
 
-/**
- * Affichage du contenu de la catégorie "Accueil" dans une page
- */
-add_shortcode ('accueil', 'gym_affiche_categorie_accueil');
-function gym_affiche_categorie_accueil () {
-	$results = $GLOBALS['wpdb']->get_results ('SELECT term_id FROM wp_terms WHERE name = "Accueil"', OBJECT);
-	query_posts ('cat='.$results[0]->term_id.'&orderby=ID');
-	return '';
-}
-
-/**
- * Affichage des horaires des cours
- * Remplace [horaires] par le tableau coloré des cours correspondant au titre de la page
- * Utilise les données entre les bbcode [horaires][/horaires] dans la fiche Horaires
- */
-add_shortcode ('horaires', 'gym_affiche_horaires');
-function gym_affiche_horaires ($atts = NULL) {
-	$titre = get_the_title (); // Titre de la page (pour les filtres)
-	$results = $GLOBALS['wpdb']->get_results ('SELECT ID, post_title, post_content FROM wp_posts WHERE post_status = "publish"', OBJECT);
-	foreach ($results AS $p) {
-		$gym_posts [cnv ($p->post_title)] = $p->ID; // Mémorise l'id de chaque post en fonction de son titre
-		$c = str_replace (array("\t","\r","\n"), '', $p->post_content);
-		$c = str_replace (array("<tr><td>","</td><td>","</td></tr>"), array('[','|',']'), $c);
-		preg_match_all('/\[([^\]]+)/', $c, $h);
-		foreach ($h[1] AS $hv) {
-			$hvs = explode ('|', $hv);
-			if (stripos ('|'.cnv($hv).'|', '|'.cnv($titre).'|')
-				&& count ($hvs) > 1)
-				$gym_cours [array_shift ($hvs)] [] = $hvs;
-			elseif (count ($hvs) > 1)
-				$gym_cours2 [array_shift ($hvs)] [] = $hvs;
-		}
-	}
-	if (!$gym_cours && $atts)
-		$gym_cours = $gym_cours2;
-
-	$r = "<div id=\"horaires\"><b>Séances</b>";
-	if (isset ($gym_cours))
-		foreach ($gym_cours AS $j => $c) {
-			$r .= "<div id=\"".strtolower($j)."\">$j<table>";
-				foreach ($c AS $cv) {
-					$r .= "<tr>";
-						foreach ($cv AS $cvk => $cvv)
-							if (cnv ($cvv) != cnv ($titre))
-								$r .= ($n = @$gym_posts [cnv ($cvv)])
-									? "<td class=\"hc$cvk\"><a href=\"?p=$n\">$cvv</a></td>"
-									: "<td class=\"hc$cvk\">$cvv</td>";
-					$r .= "</tr>";
-				}
-			$r .= "</table></div>";
-		}
-	return $r."</div>";
-}
-
-/**
- * Affichage d'images a droite de la page.
- */
-add_shortcode ('droite', 'gym_affiche_images_droite');
-function gym_affiche_images_droite ($atts = array(), $content = NULL) {
-	$img = array ();
-	preg_match_all ('/(src|mp4)="([^"]+)\.([[:alnum:]]+)"/', urldecode ($content), $match);
-	foreach ($match[3] AS $k=>$v)
-		switch ($v) {
-			case 'jpg':
-			case 'jpeg':
-			case 'gif':
-				$img [] = '<img src="'.$match[2][$k].'.'.$match[3][$k].'" />';
-				break;
-//			case 'avi':
-//				$img [] = '<embed src="'.$match[2][$k].'.'.$match[3][$k].'"></embed>';
-//				break;
-			case 'mp4':
-				$img [] =
-					'<video autoplay="true" loop="true"'.($iteration++ ? ' muted="true"' : '').'>'.
-						'<source src="'.$match[2][$k].'.'.$match[3][$k].'" type="video/mp4">'.
-							'Your browser does not support the &lt;video&gt; tag.'.
-						'</source>'.
-					'</video>';
-				break;
-			default:
-				$img [] = '<p>Format '.$match[0][$k].'" inconnu.</p>';
-		}
-	return '<div class="droite">'.implode('',$img).'</div>';
-}
-
-/**
- * Menu de gestion (Connexion, ...).
- */
-add_action ('widgets_init', create_function ('', 'return register_widget("gym_gestion");'));
-class gym_gestion extends WP_Widget {
-    function gym_gestion () {
-        parent::WP_Widget (false, $name = 'Menu de gestion GYM');	
-    }
-
-	public function widget ($args, $instance) {
-		echo $args['before_widget'];
-		echo $args['before_title'] . 'Gestion' . $args['after_title'];
-		echo '<ul>';
-			echo '<li>'; wp_loginout(); echo '</li>';
-			echo wp_register();
-			if (is_user_logged_in ('users_can_register')) {
-				$r = $GLOBALS['wpdb']->get_results ('SELECT ID FROM wp_posts WHERE post_status LIKE "publish" AND post_title = "Mode d\'emploi"', OBJECT);
-				echo '<li><a target="mode_emploi" href="?p='.$r[0]->ID.'">Mode d\'emploi</a></li>';
-				echo '<li><a target="gym_v2" href="http://chaville.gym.free.fr/v2">Prècèdent site</a></li>';
-			}
-		echo '</ul>';
-		echo $args['after_widget'];
-    }
-}
-
-/**
- * Pied de page.
- */
-add_action( 'twentyfifteen_credits', 'gym_credits');
-function gym_credits ()
+// Load correctly syles.css files
+add_action("wp_enqueue_scripts", "wp_enqueue_scripts_function");
+function wp_enqueue_scripts_function()
 {
-	echo "
-<p>Chavil'Gym est un club de la <a href=\"http://www.ffepgv.org/\">Fédération Française d'Education Physique et de Gymnastique Volontaire</a></p>
-<p>Site réalisé par <a href=\"mailto:webmestre@cavailhez.fr\">Dominique Cavailhez</a></p>
-";
-}
-/*
-	Validation:
-	<a href=\"https://validator.w3.org/check?uri=referer\">HTML5</a> -
-	<a href=\"http://jigsaw.w3.org/css-validator/check/referer\">CSS3</a> -
-	<a href=\"https://validator.w3.org/mobile/check/referer\">Mobile</a>
-*/
-
-/**
- * Redirection après login/logout.
- */
-add_filter('login_redirect', 'login_logout_redirect');
-add_filter('logout_redirect', 'login_logout_redirect');
-function login_logout_redirect() {
-	return '.';
+    wp_register_style("style", get_stylesheet_uri());
+    wp_enqueue_style("style");
 }
 
-/**
- * Fonction outil locale.
- * Purge une chaine.
- */
-function cnv ($c) {
-	// Purge NEW & CERT
-	$ce = explode ('|', $c);
-	foreach ($ce AS $k=>$v)
-		$ce[$k] = explode (' <', $v) [0];
-	$c = implode ('|', $ce);
-	
-	$c = str_replace (' - ', ' ', $c);
-	$c = strip_tags ($c);
-	$c = html_entity_decode ($c);
-	$c = strtolower ($c);
-	$c = preg_replace ('/[^a-z\|]/', '', $c);
-	return $c;
+// Réglage de l'opacité de l'éditeur
+add_action("admin_head", "admin_head_function");
+function admin_head_function()
+{
+    wp_enqueue_style(
+        "admin_css",
+        get_stylesheet_directory_uri() . "/style.css"
+    );
 }
+
+// Use global urls in block templates (as defined in wp-includes/general-template.php)
+add_shortcode("get_info", "get_info_function");
+function get_info_function($args)
+{
+    return get_bloginfo($args[0]);
+}
+
+// Lien d'édition de la page
+add_shortcode("edit_page", "edit_page_function");
+function edit_page_function()
+{
+    global $post;
+    return "<a class=\"crayon\" title=\"Modification de la page\" href=\"" .
+        get_bloginfo("url") .
+        "/wp-admin/post.php?&action=edit&post={$post->ID}\">&#9998;</a>";
+}
+
+// Menu haut de page
+add_shortcode("menu", "menu_function");
+function menu_function($args)
+{
+    global $wpdb, $post;
+
+    $pages = $wpdb->get_results("
+SELECT child.post_title, child.post_name, child.post_parent,
+  parent.post_title AS parent_title, parent.post_name AS parent_name
+FROM wpgym_posts AS parent
+JOIN wpgym_posts AS child ON parent.ID = child.post_parent
+WHERE parent.post_status = 'publish' AND child.post_status = 'publish'
+ORDER BY parent.menu_order, parent.post_title, child.menu_order, child.post_title
+");
+
+    $menu[] = '<ul class="menu">';
+    $liste[] = '<ul class="sous_pages">';
+    foreach ($pages as $p) {
+        // Au changement de sous-menu
+        if ($sous_menu != $p->post_parent) {
+            if ($sous_menu) {
+                $menu[] = "\t\t</ul>\n\t</li>";
+            }
+            $menu[] = "\t<li>\n\t\t<a onclick=\"return clickMenu(event,this)\" href='/$p->parent_name/'>$p->parent_title</a>\n\t\t<ul>";
+
+            $sous_menu = $p->post_parent;
+        }
+
+        // Pour toutes les lignes
+        $menu[] = "\t\t\t<li><a href='/$p->post_name/' title='Voir la page'>$p->post_title</a></li>";
+
+        // Affichage de sous-catégories
+        if ($sous_menu == $post->ID) {
+            $liste[] =
+                "<li><a href=\"" .
+                get_bloginfo("url") .
+                "/$p->post_name/\">$p->post_title</a></li>";
+        }
+    }
+    $menu[] = "\t\t</ul>\n\t</li>\n</ul>";
+    $liste[] = "</ul>";
+
+    return implode(PHP_EOL, $args ? $liste : $menu);
+}
+
+// Horaires
+add_shortcode("horaires", "horaires_function");
+function horaires_function()
+{
+    global $wpdb, $nom_jour;
+
+    preg_match('|/[^/]+/$|', $_SERVER["REQUEST_URI"], $page_url);
+
+    $pages_publiees = $wpdb->get_results("
+SELECT post_title, post_name, post_content, ID
+FROM wpgym_posts
+WHERE post_status = 'publish'
+");
+
+    foreach ($pages_publiees as $p) {
+        $post_names[$p->post_title] = $p->post_name;
+        $post_titles[$p->post_name] = $p->post_title;
+    }
+
+    foreach ($pages_publiees as $p) {
+        preg_match_all("|<tr>.*</tr>|U", $p->post_content, $lignes);
+        foreach ($lignes[0] as $l) {
+            preg_match_all("|<td>(.*)</td>|U", $l, $colonnes);
+            $date = explode(" ", $colonnes[1][1], 2);
+            $no_jour = array_search(strtolower(trim($date[0])), $nom_jour);
+            $edit = wp_get_current_user()->allcaps["edit_others_pages"]
+                ? "<a class=\"crayon\" title=\"Modification de la séance\" href=\"" .
+                    get_bloginfo("url") .
+                    "/wp-admin/post.php?&action=edit&post={$p->ID}\">&#9998;</a>"
+                : "";
+
+            $ligne_horaire = [
+                '<a title="Voir l\'activité" href="' .
+                get_bloginfo("url") .
+                "/{$p->post_name}/\">{$colonnes[1][0]}</a>$edit",
+
+                $date[1],
+
+                '<a title="Voir le lieu" href="' .
+                get_bloginfo("url") .
+                "/{$post_names[$colonnes[1][2]]}/\">{$colonnes[1][2]}</a>",
+
+                '<a title="Voir l\'animateur-ice" href="' .
+                get_bloginfo("url") .
+                "/{$post_names[$colonnes[1][3]]}/\">{$colonnes[1][3]}</a>",
+            ];
+
+            if (
+                $no_jour &&
+                str_contains(
+                    implode("/horaires/", $ligne_horaire),
+                    $page_url[0]
+                )
+            ) {
+                $horaires[$no_jour][$date[1]][] = $ligne_horaire;
+            }
+        }
+    }
+
+    if (count($horaires)) {
+        $cal[] = "\n<div class=\"horaires\">";
+
+        ksort($horaires);
+        foreach ($horaires as $no_jour => $jour) {
+            $rj = ["\t<table>", "\t\t<caption>{$nom_jour[$no_jour]}</caption>"];
+            ksort($jour);
+            foreach ($jour as $heure) {
+                foreach ($heure as $ligne) {
+                    $rj[] =
+                        "\t\t<tr>\n\t\t\t<td>" .
+                        implode("</td>\n\t\t\t<td>", $ligne) .
+                        "</td>\n\t\t</tr>";
+                }
+            }
+            $rj[] = "\t</table>";
+            $cal[] = implode(PHP_EOL, $rj);
+        }
+        $cal[] = "</div>";
+        return implode(PHP_EOL, $cal);
+    }
+}
+
+// Calendrier
+add_shortcode("calendrier", "calendrier_function");
+function calendrier_function()
+{
+    global $post, $annee, $nom_jour, $nom_mois;
+    date_default_timezone_set("Europe/Paris");
+
+    // Remplir des cases actives
+    preg_match_all("|<tr>.*</tr>|U", $post->post_content, $lignes);
+    foreach ($lignes[0] as $l) {
+        preg_match_all("|<td>(.*)</td>|U", $l, $colonnes);
+        if (count($colonnes) == 2 && is_numeric($colonnes[1][0])) {
+            foreach (explode(",", $colonnes[1][1]) as $jour) {
+                remplir_calendrier(
+                    $calendrier,
+                    $annee + ($colonnes[1][0] < 8 ? 1 : 0),
+                    $colonnes[1][0],
+                    $jour,
+                    "date_active"
+                );
+            }
+        }
+    }
+    // Remplir les autres cases
+    for ($j = 1; $j < 310; $j++) {
+        remplir_calendrier($calendrier, $annee, 9, $j, "");
+    }
+    // Afficher le calendrier
+    $cal = [];
+    ksort($calendrier);
+    foreach ($calendrier as $k => $v) {
+        $edit = wp_get_current_user()->allcaps["edit_others_pages"]
+            ? "<a class=\"crayon\" title=\"Modification du calendrier\" href=\"" .
+                get_bloginfo("url") .
+                "/wp-admin/post.php?&action=edit&post={$post->ID}\">&#9998;</a>"
+            : "";
+        $cal[] = "<table class=\"calendrier\">";
+        $cal[] = "<tr><td colspan=\"6\">Les {$nom_jour[$k]}s $edit</td></tr>";
+        ksort($v);
+        foreach ($v as $kv => $vv) {
+            if ($kv < 19) {
+                // N'affiche pas juillet
+                $cal[] = "<tr><td>{$nom_mois[$kv]}</td>";
+                ksort($vv);
+                foreach ($vv as $kvv => $vvv) {
+                    $cal[] = "<td class=\"$vvv\">$kvv</td>";
+                }
+                $cal[] = "</tr>";
+            }
+        }
+        $cal[] = "</table>";
+    }
+    $cal[] = "</div>";
+    return implode(PHP_EOL, $cal);
+}
+
+function remplir_calendrier(&$calendrier, $an, $mois, $jour, $set)
+{
+    global $annee;
+
+    $dateTime = new DateTime();
+    $dateTime->setDate($an, $mois, $jour);
+    $dt = explode(" ", $dateTime->format("Y N n j"));
+    $noj = $dt[1];
+    $nom = $dt[2] + ($dt[0] - $annee) * 12;
+    if (isset($calendrier[$noj]) || $set) {
+        $calendrier[$noj][$nom][$dt[3]] .= $set;
+    }
+}
+?>
