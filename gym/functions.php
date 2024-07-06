81b1486848d2044072c7427deb93ecf2d8118ca8
Dominique Cavailhez <dominique@c92.fr>
2024-07-06 10:04:47 +0200

    Position crayon modif

--- gym/functions.php
+++ gym/functions.php
@@ -4,7 +4,7 @@ if (!defined("ABSPATH")) {
   exit();
 }
 
-// Inhibe mles mails admin inutiles
+// Inhibe les mails admin inutiles
 define ("WP_DISABLE_FATAL_ERROR_HANDLER", true);
 add_filter ("auto_core_update_send_email", "__return_false"); // Disable core update emails
 add_filter ("auto_plugin_update_send_email", "__return_false"); // Disable plugin update emails
